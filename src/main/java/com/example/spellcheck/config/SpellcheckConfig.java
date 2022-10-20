8f869fdc01daf8ba4ab6471efb9609b9d7cd618c
Camila Andrade <cammiandrad@gmail.com>
2022-10-19 22:32:32 -0400

    Fix read corpus file

--- src/main/java/com/example/spellcheck/config/SpellcheckConfig.java
+++ src/main/java/com/example/spellcheck/config/SpellcheckConfig.java
@@ -4,10 +4,12 @@ import io.gitlab.rxp90.jsymspell.SymSpell;
 import io.gitlab.rxp90.jsymspell.SymSpellBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -15,17 +17,24 @@ import java.util.stream.Collectors;
 public class SpellcheckConfig {
 
     @Bean
-    public SymSpell symspell() throws IOException {
+    public SymSpell symspell() {
         return new SymSpellBuilder()
                 .setUnigramLexicon(unigrams())
                 .setMaxDictionaryEditDistance(2)
                 .createSymSpell();
     }
 
-    private Map<String, Long> unigrams() throws IOException {
-        return Files.lines(Paths.get(ClassLoader.getSystemResource("corpus.txt").getPath()))
-                .map(line -> line.split(","))
-                .collect(Collectors.toMap(tokens -> tokens[0], tokens -> Long.parseLong(tokens[1])));
+    private Map<String, Long> unigrams() {
+        final ClassPathResource resource = new ClassPathResource("corpus.txt");
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            return reader.lines()
+                    .map(line -> line.split(","))
+                    .collect(Collectors.toMap(tokens -> tokens[0], tokens -> Long.parseLong(tokens[1])));
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return Collections.emptyMap();
     }
 
 }
